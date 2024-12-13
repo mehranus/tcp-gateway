74ddd612bf218e739d5f0c33b19015315b92755f
mehran feriduni <m3hr4nus@gmail.com>
2024-12-13 19:48:07 +0330

    config for tcp service

---
+++ src/user.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { ApiConsumes } from '@nestjs/swagger';
+import { SignUpDto } from './dto/user.dto';
+import { catchError, lastValueFrom } from 'rxjs';
+
+
+@Controller('user')
+export class UserController {
+  constructor(
+    @Inject("USER_SERVICE") private readonly  userClinetService:ClientProxy
+  ) {}
+
+  @Post('signup')
+  @ApiConsumes('application/x-www-form-urlencoded')
+   async signup(@Body() signupDto:SignUpDto){
+    const respons=await lastValueFrom(
+      this.userClinetService.send("signup",signupDto).pipe((
+        catchError(err=>{
+          throw err
+        })
+      ))
+    )
+    return respons
+  }
+
+
+}
