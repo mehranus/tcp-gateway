fddbfc8b7d876c5841658b86898e8c7f327e5d52
mehran feriduni <m3hr4nus@gmail.com>
2024-12-14 00:18:22 +0330

    config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
-  await app.listen(3000);
+  const option= new DocumentBuilder()
+  .setTitle("NestJS-microService")
+  .setVersion("v1")
+  .addBearerAuth({
+    type:'http',
+    bearerFormat:"JWT",
+    in:'header',
+    scheme:'bearer'
+  },
+  "Authorization"
+).build()
+const document=SwaggerModule.createDocument(app,option)
+SwaggerModule.setup('/',app,document)
+  await app.listen(4000,()=>{
+    console.log('gateway: http://localhost:4000')
+  });
 }
 bootstrap();
